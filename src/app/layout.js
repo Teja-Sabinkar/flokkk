09fa7d433f13c93997ac4aad9f7925b580c8b05d
Teja-Sabinkar <sabinkar2304@gmail.com>
2025-05-17 20:21:24 +0100

    hosting - Patch - 1.6 - WA - just pages. no customs

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,6 @@
 import './globals.css';
 import './fonts.css';
-
+import { Analytics } from '@vercel/analytics/react';
 
 export const metadata = {
   title: 'Turtle2.0',
@@ -13,13 +13,14 @@ export default function RootLayout({ children }) {
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link 
-          href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&display=swap" 
-          rel="stylesheet" 
+        <link
+          href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&display=swap"
+          rel="stylesheet"
         />
       </head>
       <body>
         {children}
+        <Analytics />
       </body>
     </html>
   )
